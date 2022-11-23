661439b53194d8b50328152e73279612147351fb
Anton Rudnev <anton.rudnev@gmail.com>
2022-11-23 11:47:44 -0500

    Daylight saving processed

--- web/prg/httpd_prg_delay.sh
+++ web/prg/httpd_prg_delay.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+tz="America/New_York"
+
 while read time_delta
 do
     if [ -z "$time_delta" ]
     then
-        now=$(TZ=EST date +%H)
-        if [ "$now" -ge 2 -a "$now" -lt 12 ]
+        now=$(date --utc)
+        tz_dif=$(($(TZ=Europe/Moscow date -d "$now" +%:::z)-$(TZ=$tz date -d "$now" +%:::z)))
+        now_hr=$(TZ=$tz date -d "$now" +%H)
+        if [ "$now_hr" -ge 2 -a "$now_hr" -lt 12 ]
         then
-            echo $(date -d "-8 hours" +%Y-%m-%dT%H:%M:%S)
-        elif [ "$now" -ge 12 -a "$now" -lt 16 ]
+            echo $(date -d "-$((tz_dif+1)) hours" +%Y-%m-%dT%H:%M:%S)
+        elif [ "$now_hr" -ge 12 -a "$now_hr" -lt 16 ]
         then
-            echo $(date -d "-7 hours" +%Y-%m-%dT%H:%M:%S)
+            echo $(date -d "-$tz_dif hours" +%Y-%m-%dT%H:%M:%S)
         else
-            echo $(date -d "-6 hours" +%Y-%m-%dT%H:%M:%S)
+            echo $(date -d "-$((tz_dif-1)) hours" +%Y-%m-%dT%H:%M:%S)
         fi
     else
         echo $(date -d "$time_delta" +%Y-%m-%dT%H:%M:%S)
