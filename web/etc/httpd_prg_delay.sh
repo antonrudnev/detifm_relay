4a6674e623777739602d99623601cbcc8b1499b1
Anton Rudnev <anton.rudnev@gmail.com>
2022-11-24 12:35:16 -0500

    Delay updated

--- web/etc/httpd_prg_delay.sh
+++ web/etc/httpd_prg_delay.sh
@@ -7,17 +7,18 @@ do
     if [ -z "$time_delta" ]
     then
         now=$(date --utc)
-        tz_dif=$(($(TZ=Europe/Moscow date -d "$now" +%:::z)-$(TZ=$tz date -d "$now" +%:::z)))
         now_hr=$(TZ=$tz date -d "$now" +%H)
+        tz_dif=$(($(TZ=Europe/Moscow date -d "$now" +%:::z)-$(TZ=$tz date -d "$now" +%:::z)))
         if [ "$now_hr" -ge 0 -a "$now_hr" -lt 9 ]
         then
-            echo $(date -d "-$((tz_dif+1)) hours" +%Y-%m-%dT%H:%M:%S)
+            shft=$((tz_dif+1))
         elif [ "$now_hr" -ge 17 -a "$now_hr" -lt 21 ]
         then
-            echo $(date -d "-$((tz_dif-1)) hours" +%Y-%m-%dT%H:%M:%S)
+            shft=$((tz_dif-1))
         else
-            echo $(date -d "-$((tz_dif)) hours" +%Y-%m-%dT%H:%M:%S)
+            shft=$((tz_dif))
         fi
+        echo $(date -d "-$shft hours" +%Y-%m-%dT%H:%M:%S)
     else
         echo $(date -d "$time_delta" +%Y-%m-%dT%H:%M:%S)
     fi
