2cc73bd41638617980c186d1098f06399e7a2a12
Anton Rudnev <anton.rudnev@gmail.com>
2022-11-24 02:23:24 +0000

    Updated time shift

--- web/etc/httpd_prg_delay.sh
+++ web/etc/httpd_prg_delay.sh
@@ -9,14 +9,14 @@ do
         now=$(date --utc)
         tz_dif=$(($(TZ=Europe/Moscow date -d "$now" +%:::z)-$(TZ=$tz date -d "$now" +%:::z)))
         now_hr=$(TZ=$tz date -d "$now" +%H)
-        if [ "$now_hr" -ge 2 -a "$now_hr" -lt 12 ]
+        if [ "$now_hr" -ge 0 -a "$now_hr" -lt 9 ]
         then
             echo $(date -d "-$((tz_dif+1)) hours" +%Y-%m-%dT%H:%M:%S)
-        elif [ "$now_hr" -ge 12 -a "$now_hr" -lt 16 ]
+        elif [ "$now_hr" -ge 17 -a "$now_hr" -lt 21 ]
         then
-            echo $(date -d "-$tz_dif hours" +%Y-%m-%dT%H:%M:%S)
-        else
             echo $(date -d "-$((tz_dif-1)) hours" +%Y-%m-%dT%H:%M:%S)
+        else
+            echo $(date -d "-$((tz_dif)) hours" +%Y-%m-%dT%H:%M:%S)
         fi
     else
         echo $(date -d "$time_delta" +%Y-%m-%dT%H:%M:%S)
